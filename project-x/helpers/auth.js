091f4c354e43391504c92bf2e53f0fe82ef00037
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-05 17:14:12 +0200

    added helper functions and passportjs

---
+++ project-x/helpers/auth.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+  //this method saves the current user in the global scope
+  //so it can be accessed  without passing it while rendering the page
+  setCurrentUser: (req, res, next)=> {
+    if(req.isAuthenticated()){
+      res.locals.currentUser = req.user;
+      res.locals.isUserLoggedIn = true;
+
+    } else {
+      res.locals.isUserLoggedIn = false;
+    }
+    next();
+  },
+  //this method checks if a user is logged in, it also checks
+  //the authorization level of the user
+  //the third argument if is set to 1 locks the authorization level only
+  //for that user type, i.e only owners can sell products -> checkLoggedIn(redirectPath, "Owner", 1);
+
+  checkLoggedIn: (redirectPath, authLevel = "User", exclusive = 0) => {
+
+    return (req, res, next)=> {
+      const roles = ['User','Owner', 'Professional', 'Admin'];
+
+      if(req.isAuthenticated()){
+        const checkLevel = roles.indexOf(authLevel);
+        const currentLevel = roles.indexOf(req.user.role);
+        if(exclusive)
+          if(currentLevel == checkLevel) next();
+          else res.redirect(redirectPath);
+        else
+          if(currentLevel >= checkLevel) next();
+          else res.redirect(redirectPath);
+      }
+      else res.redirect(redirectPath);
+
+    };
+  },
+
+};
