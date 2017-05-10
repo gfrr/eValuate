191c5b5a2189b0eafebdff913d69337b3a31e448
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-10 16:05:28 +0200

    added become an expert

--- project-x/routes/dashboardController.js
+++ project-x/routes/dashboardController.js
@@ -4,6 +4,7 @@ const passport = require("../helpers/passport");
 const User = require("../models/user");
 const flash = require('connect-flash');
 const Item = require("../models/item");
+const Expert = require("../models/expert");
 const auth = require("../helpers/auth");
 
 //to go to dashboard  personal data and chek if is logged in
@@ -12,6 +13,45 @@ router.get('/dashboard', auth.checkLoggedIn("/logout"), (req, res, next)=> {
     if(err) next(err);
     res.render('user/dashboard', {user: users[0]});
   });
+});
+
+router.post("/dashboard", auth.checkLoggedIn("/logout"), (req, res, next)=> {
+  User.findById(req.user._id, (err, user)=>{
+    if(err) next(err);
+    if (user.role !== "Admin" || user.role !== "Professional"){
+      user.role = "Professional";
+      user.save();
+      let focus;
+      if(typeof(req.body.focus) == "string") focus = [req.body.focus];
+      else focus = req.body.focus;
+      const expertInfo = {
+        cv: req.body.cv,
+        photo: req.body.photo,
+        website: req.body.website,
+        focus,
+        pending: [],
+        completed: [],
+        userId: user._id
+      };
+      const expert = Expert(expertInfo);
+      expert.save();
+      console.log(user);
+      console.log(expert);
+      res.redirect("/dashboard");
+    }
+
+
+  });
+
+  // const expertSchema = new Schema({
+  //   cv: String,
+  //   photo: String,
+  //   website: String,
+  //   focus: [], // i.e. Stamps, Coins etc. It can be one or more.
+  //   pending: [],
+  //   completed: [],
+  //   userId: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+  // });
 });
 
 // Still need to secure the route!! remember before deploying
@@ -24,6 +64,8 @@ router.get("/dashboard/remove/:item_id", auth.checkLoggedIn("/logout"), (req, re
     res.redirect("/dashboard");
   });
 });
+
+
 
 /*
 if(req.user._id == String(item[0].userId) || req.user.role == "Admin") res.render("auth/edit-item", {item: item[0]});
