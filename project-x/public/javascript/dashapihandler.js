7b4a4518ae9e455ec5ec03b75804f5306e49e743
Andrea Martins <marlit8@gmail.com>
2017-05-10 09:09:27 +0200

    buttomofferadded

--- project-x/public/javascript/dashapihandler.js
+++ project-x/public/javascript/dashapihandler.js
@@ -25,6 +25,7 @@ function test(result){
            <a href="/items/${result._id}" class="btn btn-default" role="button">Details</a>
            <a href="/items/${result._id}/edit" class="btn btn-default" role="button">Edit</a>
            <a href="/dashboard/remove/${result._id}" class="btn btn-danger" role="button">Remove</a>
+           <a href="/items/${result._id}" class="btn btn-default" role="button">Offers</a>
          </p>
        </div>
 
