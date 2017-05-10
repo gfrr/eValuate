a95fee55d63f227d9c828565ff5313624548681b
Andreas Karpasitis <karpas@Andreass-MacBook-Pro.local>
2017-05-10 16:05:17 +0200

    added offer list for each users item, shown in the dashboard

--- project-x/public/javascript/dashapihandler.js
+++ project-x/public/javascript/dashapihandler.js
@@ -16,27 +16,55 @@ $(document).ready(()=>{
 
 function test(result, target){
   console.log(target.toLowerCase());
+  let offers = [];
+  if (result.currentOffers.length > 0) {
+    result.currentOffers.forEach((offer)=>{
+      console.log(offer.amount);
+      offers.push(offer.amount);
+    });
+  } else {
+    offers = [`<p> No Current Offers </p>`];
+  }
  $(`#${target.toLowerCase()}`).append(`<div class="row">
 
+ <div class="row">
    <div class="col-sm-6 col-md-4">
      <div class="thumbnail">
        <img src="${result.images[0].image}">
        <div class="caption">
-         <h3>${result.title}</h3>
+          <h3>${result.title}</h3>
          <p>${result.keywords}</p>
          <p>
            <a href="/items/${result._id}" class="btn btn-default" role="button">Details</a>
            <a href="/items/${result._id}/edit" class="btn btn-default" role="button">Edit</a>
            <a href="/dashboard/remove/${result._id}" class="btn btn-danger" role="button">Remove</a>
-           <a href="/items/${result._id}" class="btn btn-default" role="button">Offers</a>
          </p>
        </div>
 
+       </div>
      </div>
-   </div>
-
+     <div class="current-offers col-sm-4 col-md-4">
+        <h3> Offers </h3>
+        ${offers.join("<br><br>")}
+       </div>
+       </div>
+       </div>
  </div>`);
+
+
   click();
+  $(".offers-button").click(function(e){
+    console.log("test: " + e);
+    e.preventDefault();
+    $('body').append($('<div/>')
+      .attr("id", "popup-offer")
+      .addClass("popup")
+      .append("<span/>")
+        .html(`<script src="/javascript/popup.js"></script>
+        <a class="closelink" href="#"><img class="close-icon" src="/images/close.svg" alt=""></a><br>
+        <img class="cvimage" src="/images/cv.png" alt="">`));
+  });
+
 }
 
 function click(id){
