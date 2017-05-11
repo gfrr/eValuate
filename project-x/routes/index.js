13defd85de84e954b269e78f3cc083d30f2b295a
Andrea Martins <marlit8@gmail.com>
2017-05-11 14:30:46 +0200

    buttom delete

--- project-x/routes/index.js
+++ project-x/routes/index.js
@@ -98,7 +98,7 @@ router.get('/search/:word?', (req, res) => {
    if (String(type) == "Users"){
      console.log("1");
   		User.find({lastName: word}, (error, user) => {
-        if (error) res.render("user/showusers");
+        // if (error) res.render("user/showusers");
         if(!user[0])res.redirect("/users");
 
   			// if (error) res.status(500).json({message: error});
@@ -112,7 +112,7 @@ if (String(type) == "Items"){
   word = word.split("%20").join(" ");
   console.log(word);
    Item.find({title: String(word)}, (error, item) => {
-     if (error) res.render("item/showitems");
+    //  if (error) res.render("item/showitems");
      if(!item[0])res.redirect("/items");
     //  if (error) res.status(500).json({message: error});
      else console.log(item);
