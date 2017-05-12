c3389f92a319fb5257a0a58e58566cec1c2070ab
Giuseppe Fierro <fierro.gpp@gmail.com>
2017-05-12 08:46:59 +0200

    added avatars for everyone, apikey hotfix

--- bin/seeds.js
+++ bin/seeds.js
@@ -6,6 +6,10 @@ const User = require("../models/user");
 const Item = require("../models/item");
 const Feedback = require("../models/feedback");
 const Expert = require("../models/expert");
+const dotenv = require('dotenv');
+
+dotenv.config();
+dotenv.load();
 
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -23,6 +27,7 @@ function createUserType(number, userType,  password = "test" ){
       firstName: name.split(" ")[0],
       lastName: name.split(" ")[1],
       email: faker.internet.email(name),
+      avatar: faker.internet.avatar(),
       password: hashPass,
       address: {
         street: address.streetC,
---
+++ public/javascript/experts.js
@@ -0,0 +1,5 @@
+const dbAPI = new APIHandler("http://localhost:3000");
+
+$(document).ready(()=>{
+  // console.log(users);
+});
