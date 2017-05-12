9b5c6dfe97651696a375ff456a98c4a1fbd17fa9
Andreas Karpasitis <karpas@Andreass-MacBook-Pro.local>
2017-05-12 10:48:33 +0200

    favicon serve removed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -31,8 +30,6 @@ mongoose.connect(process.env.MONGODB_URI);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(expressLayouts);
 app.set('layout', 'layouts/main-layout');
 app.use(logger('dev'));
